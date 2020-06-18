a14ac6b0e41714884bc3b83b039f3e1885a47bc1
luis <lruise.1@gmail.com>
2020-06-17 22:39:43 -0400

    otro error

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const app=require('express');
+const express=require('express');
 const serveStatic=require('serve-static');
 const path=require('path');
 
-app.express();
+const app=express();
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')));
 
